cef967191bc4caba3d63fec67a91b359f0a808a3
Quentin Durant <quentin.durant49@orange.fr>
2025-12-18 09:28:17 +0100

    push websocket

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Test Page' , message: 'le caca est cuit '});
+router.get('/test', function (req, res, next) {
+  res.render('test', { title: 'Test Page', message: 'le caca est cuit ' });
 });
+
+router.get('/login', function (req, res, next) {
+  res.render('loginsimple', { title: 'Login Page' });
+});
+
+router.post('/login', function (req, res, next) {
+  const { username, password } = req.body;
+  if (username === 'admin' && password === 'admin') {
+    res.render('test', { title: 'page admin', message: 'Bienvenue admin' });
+  } else if (username === 'mds' && password === 'mds') {
+    res.render('test', { title: 'page élève', message: 'Bienvenue élève' });
+  } else {
+    res.send('Identifiants incorrects');
+  }
+});
+
+router.get('/tchat', function (req, res, next) {
+  res.render('tchat', { title: 'Chat en temps réel' });
+});
+
+
 
 module.exports = router;
